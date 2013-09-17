50ab89f3d4f6943946f1b7ce6fdb79094def0d89
vijay <vijay@squaretrade.com>
2013-09-17 06:40:50 -0700

    refactored

--- algorithm/src/test/java/org/interview/algorithms/bit/CalculateParity.java
+++ algorithm/src/test/java/org/interview/algorithms/bit/CalculateParity.java
@@ -3,16 +3,18 @@ package org.interview.algorithms.bit;
 public class CalculateParity {
 
 	private static String calcuateParity(long x){
-		int result = 0;
+		long result = 0;
 		  while (x != 0) {
-		    result ^= (x & 1);
+//		    result ^= (x & 1);
+		    result = result ^ (x & 1);
 		    x >>= 1;
 		  }
 		  return result == 1 ? "odd" : "even";
 	}
 	public static void main(String[] args) {
 		System.out.println("1 = "+calcuateParity(1l));
-		System.out.println("31 = "+calcuateParity(3l));
+		System.out.println("3 = "+calcuateParity(3l));
+		System.out.println("7 = "+calcuateParity(7l));
 		System.out.println("21 = "+calcuateParity(21l));
 		System.out.println("30 = "+calcuateParity(30l));
 	}
